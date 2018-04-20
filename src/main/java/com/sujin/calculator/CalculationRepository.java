f56255d5864059226c9f5d6208bf18c16a185576
NIIIII99 <mystilltime@naver.com>
2018-04-20 16:04:25 +0900

    Create CalculationRepository.java

---
+++ src/main/java/com/sujin/calculator/CalculationRepository.java
@@ -0,0 +1,9 @@
+package com.sujin.calculator;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Calculation repository.
+ */
+public interface CalculationRepository extends CrudRepository<Calculation, Integer>{
+}
